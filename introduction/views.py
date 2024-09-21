8cdcf0113ce610d2e8e9b9d23648bf59e7e55fc1
Gaoyuan Xin <gaoyuanxi4@gmail.com>
2024-09-21 22:26:12 +0800

    添加Readme

--- introduction/views.py
+++ introduction/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the introduction index.")
+    return HttpResponse("<h1> Hello, world. You're at the introduction index.</h1>")
