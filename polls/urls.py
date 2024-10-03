a5bccc582efef9b904c509954a888ca4e248bfda
Gaoyuan Xin <gaoyuanxi4@gmail.com>
2024-10-04 02:08:35 +0800

    添加视图模版相关的操作方法

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="detail"), #  question_id会以变量的方式传递给视图函数
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
