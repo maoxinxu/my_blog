f931f14b0de8df72d6cb712d0aa176163fce1e5f
Gaoyuan Xin <gaoyuanxi4@gmail.com>
2024-10-04 16:08:21 +0800

    支持url自动推导
    	1、添加命名空间
    	2、使用模板语言推导URL

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:question_id>/", views.detail, name="detail"), #  question_id会以变量的方式传递给视图函数
